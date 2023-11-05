f040b13ec90d46f62f27ae2f11d91f1c6fc1c792
bWoodyR <r.blackwoody@gmail.com>
2023-11-05 10:01:16 +0100

    adding steam to socials

--- src/components/Socials/socialsList.tsx
+++ src/components/Socials/socialsList.tsx
@@ -1,11 +1,11 @@
-import { FaLinkedin, FaGithub, FaClipboardUser } from "react-icons/fa6";
+import { FaLinkedin, FaGithub, FaClipboardUser, FaSteam } from "react-icons/fa6";
 // import Resume from "../../assets/Resume/Resume.pdf";
 
 export const socials = [
   {
     title: "Linkedin",
     icon: <FaLinkedin />,
-    color: "steelblue",
+    color: "#0A66C2",
     url: "https://www.linkedin.com/in/roman-ďurek-b9a68b282",
   },
   {
@@ -13,6 +13,12 @@ export const socials = [
     icon: <FaGithub />,
     color: "#010409",
     url: "https://github.com/bWoodyR",
+  },
+  {
+    title: "Steam",
+    icon: <FaSteam />,
+    color: "#171D25",
+    url: "https://steamcommunity.com/id/bWoody/",
   },
   {
     title: "Resume",
